437982ccbbbb461bc9b4a518101d69dc52aff97f
TimberedQuasar <szymon.j.jancewicz@gmail.com>
2025-11-24 23:16:56 +0100

    adding_banner

--- lib/features/countdown/countdown_banner.dart
+++ lib/features/countdown/countdown_banner.dart
@@ -2,29 +2,19 @@ import 'package:flutter/material.dart';
 
 class CountdownBanner extends StatelessWidget {
   const CountdownBanner({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: const Text(
+        'Countdown Banner',
+        style: TextStyle(color: Colors.white, fontSize: 16),
+      ),
+    );
   }
-
-//  @override
-//  Widget build(BuildContext context) {
-//    return Container(
-//      padding: const EdgeInsets.all(16.0),
-//      decoration: BoxDecoration(
-//        color: Colors.deepPurple,
-//        borderRadius: BorderRadius.circular(8.0),
-//      ),
-//      child: const Text(
-//        'Countdown to Sylwester!',
-//        style: TextStyle(
-//          color: Colors.white,
-//          fontSize: 20,
-//          fontWeight: FontWeight.bold,
-//        ),
-//      ),
-//    );
-//  }
-}}
+}
